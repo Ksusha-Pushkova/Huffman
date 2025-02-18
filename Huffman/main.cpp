a8652f244801e92cfe8ddf1d170ef14795d0e965
Ksusha-Pushkova <puskovaksusa34@gmail.com>
2025-02-18 22:15:03 +0300

    объявление переменных и задание локали

---
+++ Huffman/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <codecvt>
+using namespace std;
+
+int main() {
+	string file_path;
+	string operation;
+	string output_file_path;
+	setlocale(LC_ALL, "Russian");
+
+}
